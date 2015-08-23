d403033181219cae400cc160c5b9a7ff56214ba5
BigWildWings <kirbytyardley@gmail.com>
2015-08-22 19:54:31 -0600

    nav updates, added vid modal, mobile styles

--- js/master.js
+++ js/master.js
@@ -21,12 +21,32 @@ $(document).ready(function() {
 
 });
 
+// Inefficient Nav Links JS, but it works for now
+
+function moveTo(e) {
+  e.preventDefault();
+
+  $(".main").moveTo(event.target.id);
+
+  $('#js-navigation-menu').slideToggle(function(){
+    if($('#js-navigation-menu').is(':hidden')) {
+      $('#js-navigation-menu').removeAttr('style');
+    }
+  });
+
+  // toggles class of hamburger, animating it into an X
+  $('.navigation-menu-button').toggleClass('open');
+
+  // toggles class of li.nav-link, fading them in 
+  $('.nav-link').toggleClass('show'); 
+}
+
 $(document).ready(function() {
   $(".main").onepage_scroll({
      sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
      easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                       // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-     animationTime: 700,              // AnimationTime let you define how long each section takes to animate
+     animationTime: 400,              // AnimationTime let you define how long each section takes to animate
      pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
      updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
      beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
@@ -37,5 +57,24 @@ $(document).ready(function() {
                                       // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
                                       // the browser's width is less than 600, the fallback will kick in.
      direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+  });
+});
+
+// Modal JS
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
   });
 });
